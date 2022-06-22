e1645c8822c010f4bbfa8e9a3b9a46e0d40fe80e
JustWaveThings <flove101@gmail.com>
2022-06-22 18:05:45 -0400

    Remove eventListeners works. Game stops at win condition

--- rps.js
+++ rps.js
@@ -1,52 +1,5 @@
-/*    back to basics.... pseudocode and break it down into discrete parts
-
-objectives
-
-    overall goal - have a functional, console based rock paper scissors game that has 5 rounds and declares a winner at the end 
-
-    Things needed to meet overall goal:
-        - variables initalized for computer and player score
-        - check at the end of the round if there's a winner of the game.
-        - increment score at the end of the round 
-        - keep and display score after each round.
-        - random computer selection from options
-        - prompt for player to pick an option  
-        - fix player input to meet strict equality (bonus) not let player  make an invalid selection so no round is forfeit.  --  like an if statement that if the input isn't rock, paper, or scissors, it doesn't accept it and makes them input again before processing the input into the game. 
-        - logic that compares computer choice to player choice to decide winner of round
-        - bonus: contextually appropriate statements that add some fun to the winning and losing each round. 
 
 
-        psuedocode 
-
-        init compScore 0
-        init playerScore 0
-
-        if playerScore === 5 or compScore === 5 
-            then gameOver 
-            else
-            playRound
-
-        playRound gets parameters  compChoice and playerChoice
-            if playerChoice is rock  & compChoice is scissors or not paper
-                player wins
-                increment playerScore +1
-                return console log  ( Player wins - you chose playerchoice and computer chose compchoice )
-            else 
-                player loses
-                increment compScore +1
-                return console log ( player loses - you chose playerChoice and computer chose compChoice.)
-
-            repeat for playerChoice scissors and paper
-            ...
-
-            gameOver
-                if compScore === 5 
-                    you lost the game.  reload page to try your luck next time 
-                else if playerScore === 5
-                    you won the game. reload page to try to win again. 
-
-            
-*/
 
 // initalized global variables
 let compScore = 0;
@@ -119,27 +72,30 @@ function roundScore(round = roundA, playerS = playerScore, compS = compScore) {
 }
 
 function endGame (playerS = playerScore, compS = compScore) {
-    if (compS <= 4 && playerS <= 4) {
-        playRound();
-    } else {
-        if (compS > 4) {
-            console.log("Computer Won. Reload page to try again.");
-            return ;
-        } else {
-            console.log("You beat the computer. Way to go! Reload page to try your luck again.");
-            return;
-        }
-    }
+  if (compS > 4) {
+    console.log("Computer Won. Reload page to try again.");
+    return "end";
+  } else if (playerS > 4) {
+    console.log("You beat the computer. Way to go! Reload page to try your luck again.");
+    return "end";
+  }
 }
+  
+const buttons = document.querySelectorAll("button");
 
-function playerClick() {
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-            playRound(compChoice(), button.id);
-        });
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    playRound(button.id, compChoice());
+    removeListeners();
   });
+});
+
+function removeListeners(game = endGame()) {
+  if (game === 'end') {
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!the true condition of removeListeners ran");
+    const elem = document.getElementById("playerinput");
+    elem.replaceWith(elem.cloneNode(true));
+  }
 }
-
-playerClick();
-
